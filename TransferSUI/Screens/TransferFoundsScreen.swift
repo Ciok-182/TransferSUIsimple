7b6ab07703c67a32a9b54b7b1b5ea910384b48f8
Jorge Encinas <jorge.encinas@devant.com.mx>
2021-09-19 19:55:41 -0500

    TransferFoundsAccountSelectionView  acctionSheetButtons

--- TransferSUI/Screens/TransferFoundsScreen.swift
+++ TransferSUI/Screens/TransferFoundsScreen.swift
@@ -8,10 +8,89 @@
 import SwiftUI
 
 struct TransferFoundsScreen: View {
+    
+    
+    @ObservedObject private var transferFoundsVM = TransferFoundsViewModel()
+    
+    @State private var showSheet: Bool = false
+    @State private var isFromAccount: Bool = false
+    
+    
+    var acctionSheetButtons: [Alert.Button]{
+        var actionButtons = transferFoundsVM.filteredAccounts.map{ account in
+            Alert.Button.default(Text("\(account.name) (\(account.accountType))")){
+                
+                if isFromAccount {
+                    transferFoundsVM.fromAccount = account
+                } else {
+                    transferFoundsVM.toAccount = account
+                }
+                
+            }
+        }
+        
+        actionButtons.append(.cancel())
+        
+        return actionButtons
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            AccountListView(accounts: transferFoundsVM.accounts)
+                .frame(height: 300)
+            
+            TransferFoundsAccountSelectionView(transferFoundsVM: transferFoundsVM, showSheet: $showSheet, isFromAccount: $isFromAccount)
+            Spacer()
+                .onAppear{
+                    self.transferFoundsVM.populateAccounts()
+                }
+                
+                .actionSheet(isPresented: $showSheet){
+                    ActionSheet(title: Text("Transfer Founds"), message: Text("Choose an account"), buttons: acctionSheetButtons)
+                }
+            
+        }.navigationTitle("Transfer Founds")
+        .embedInNavigationView()
     }
 }
+
+struct TransferFoundsAccountSelectionView: View {
+    
+    @ObservedObject var transferFoundsVM: TransferFoundsViewModel
+    @Binding var showSheet: Bool
+    @Binding var isFromAccount: Bool
+    
+    
+    var body: some View {
+        VStack(spacing: 10){
+            Button("From \(transferFoundsVM.fromAccountType)"){
+                self.showSheet = true
+                self.isFromAccount = true
+                
+                
+                
+                
+            }.padding().frame(maxWidth: .infinity)
+            .background(Color.green)
+            .foregroundColor(Color.white)
+            
+            Button("To \(transferFoundsVM.toAccountType)"){
+                self.showSheet = true
+                self.isFromAccount = false
+                
+                
+                
+            }.padding().frame(maxWidth: .infinity)
+            .background(Color.green)
+            .foregroundColor(Color.white)
+            .opacity(self.transferFoundsVM.fromAccount == nil ? 0.5 : 1)
+            .disabled(self.transferFoundsVM.fromAccount == nil ? true : false)
+        }.padding()
+        
+    }
+}
+
+
 
 struct TransferFoundsScreen_Previews: PreviewProvider {
     static var previews: some View {
